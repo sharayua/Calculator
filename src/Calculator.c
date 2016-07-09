bdf53eac48b0499e52e3895771bc9202011d96d7
sharayu <sharayu.amritkar@gmail.com>
2016-07-09 14:33:29 +0530

    Refactored code!

--- src/Calculator.c
+++ src/Calculator.c
@@ -13,32 +13,48 @@
 
 int add(int num1, int num2);
 int sub(int num1, int num2);
+void printMenu();
+int getInput(int num);
+int getChoice(int op);
+
 int main(void) {
 
 	int num1,num2;
-	int res;
-	int ch;
+	int res;int op;
 
+	printMenu();
+	op = getChoice(op);
 
-	printf("\n========MENU============\n1,Add\n2.Subtract");
-	printf("\nEnter choice :: ");
-	scanf("%d", &ch);
+	num1 = getInput(num1);
+	num2 = getInput(num2);
 
-	printf("\nEnter num 1 :: ");
-	scanf("%d", &num1);
-
-	printf("\nEnter num  :: ");
-	scanf("%d", &num2);
-
-	if(ch==1)
+	if(op==1)
 	res = add(num1, num2);
-	else if(ch==2)
+	else if(op==2)
 	res = add(num1, num2);
 	else
 	printf("\nWrong choice!");
 
 	printf("\nResult :: %d",res);
 	return EXIT_SUCCESS;
+}
+
+int getChoice(int op) {
+	printf("\nEnter choice :: ");
+	scanf("%d", &op);
+	return op;
+}
+
+void printMenu()
+{
+	printf("\n========MENU============\n1,Add\n2.Subtract");
+
+}
+
+int getInput(int num) {
+	printf("\nEnter num 1 :: ");
+	scanf("%d", &num);
+	return num;
 }
 
 int add(int num1, int num2)
@@ -52,3 +68,5 @@ int sub(int num1, int num2)
 	int diff = num1 - num2;
 	return diff;
 }
+
+
